f8e0a951f21476f4cfd634e751bfbab847269c93
Abdulrahman Sarhan <abdulrahmanayman1@gmail.com>
2026-04-28 11:46:51 -0300

    yes.

--- cb.h
+++ cb.h
@@ -1,41 +1,41 @@
 #ifndef CB_H
 #define CB_H
 
-#define STR_LIT(x)(string){.value = x, .len = sizeof(x) - 1}
+#define CB_STRING(x) (cb_string){.value = x, .len = sizeof(x) - 1}
 
 #define cb_rebuild_yourself(argc, argv) cb_rebuild_yourself_impl((argc), (argv), __FILE__)
 
 #ifndef cb_cc
-#  if _WIN32
-#    if defined(__GNUC__)
-#       define cb_cc(cmd) cb_append(cmd, STR_LIT("cc"))
-#    elif defined(__clang__)
-#       define cb_cc(cmd) cb_append(cmd, STR_LIT("clang"))
-#    elif defined(_MSC_VER)
-#       define cb_cc(cmd) cb_append(cmd, STR_LIT("cl.exe"))
-#    elif defined(__TINYC__)
-#       define cb_cc(cmd) cb_append(cmd, STR_LIT("tcc"))
-#    endif
-#  else
-#    define cb_cc(cmd) cb_append(cmd, STR_LIT("cc"))
-#  endif
+#if _WIN32
+#if defined(__GNUC__)
+#define cb_cc(cmd) cb_append(cmd, CB_STRING("cc"))
+#elif defined(__clang__)
+#define cb_cc(cmd) cb_append(cmd, CB_STRING("clang"))
+#elif defined(_MSC_VER)
+#define cb_cc(cmd) cb_append(cmd, CB_STRING("cl.exe"))
+#elif defined(__TINYC__)
+#define cb_cc(cmd) cb_append(cmd, CB_STRING("tcc"))
+#endif
+#else
+#define cb_cc(cmd) cb_append(cmd, CB_STRING("cc"))
+#endif
 #endif // cb_cc
 
 typedef struct {
     char *value;
     size_t len;
     size_t capacity;
-}command;
+} command;
 
 typedef struct {
     char *value;
-    size_t len;
-}string;
+    int len;
+} cb_string;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-void cb_append(command *cmd, string str_to_append);
+void cb_append(command *cmd, cb_string str_to_append);
 int cb_needs_rebuild(const char *binary_path, const char **source_paths, size_t source_paths_count);
 int cb_run(command *cmd);
 void cb_run_async(command *cmd);
@@ -49,28 +49,30 @@ void cb_reset(command *cmd);
 
 #ifdef CB_IMPLEMENTATION
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shellapi.h>
 
 typedef struct {
     HANDLE process_handle;
     HANDLE thread_handle;
-}cb_process;
+} cb_process;
 
 typedef struct {
     cb_process procs[1024];
-    size_t procs_count;
-}cb_context;
+    int procs_count;
+} cb_context;
 cb_context cb_ctx;
 
-void cb_append(command *cmd, string str_to_append) {
+void cb_append(command *cmd, cb_string str_to_append) {
 
     /* lengths of both strings do not include the null terminator */
-    string prefix = STR_LIT("cmd.exe /c");
+    cb_string prefix = CB_STRING("cmd.exe /c");
 
-    if(!cmd->len) {
+    if (!cmd->len) {
         size_t intitial_cap = str_to_append.len + 1 + prefix.len;
         cmd->value = malloc(intitial_cap);
         cmd->capacity = intitial_cap;
@@ -80,7 +82,7 @@ void cb_append(command *cmd, string str_to_append) {
     }
 
     size_t required = cmd->len + str_to_append.len + 2; /* to account for space and null term */
-    if(cmd->capacity < required) {
+    if (cmd->capacity < required) {
         cmd->value = realloc(cmd->value, required);
         cmd->capacity = required;
     }
@@ -88,7 +90,7 @@ void cb_append(command *cmd, string str_to_append) {
     char *cb_end = &cmd->value[cmd->len];
     cb_end[0] = ' ';
     cb_end++;
-    for(int i = 0; i < str_to_append.len; i++) {
+    for (int i = 0; i < str_to_append.len; i++) {
         cb_end[i] = str_to_append.value[i];
     }
     cmd->len += str_to_append.len + 1; /* to account for additional space */
@@ -96,11 +98,12 @@ void cb_append(command *cmd, string str_to_append) {
 }
 
 int cb_run(command *cmd) {
-    STARTUPINFOA si = { sizeof(si) };
+    STARTUPINFOA si = {0};
+    si.cb = sizeof(si);
     PROCESS_INFORMATION pi;
 
     printf("[INFO] %s\n", cmd->value);
-    if (!CreateProcessA( NULL, cmd->value, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+    if (!CreateProcessA(NULL, cmd->value, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
         printf("[ERROR] CreateProcess failed: %lu\n", GetLastError());
         return 0;
     }
@@ -118,7 +121,8 @@ int cb_run(command *cmd) {
 }
 
 void cb_run_async(command *cmd) {
-    STARTUPINFOA si = { sizeof(si) };
+    STARTUPINFOA si = {0};
+    si.cb = sizeof(si);
     PROCESS_INFORMATION pi;
 
     printf("[INFO] %s\n", cmd->value);
@@ -132,17 +136,17 @@ void cb_run_async(command *cmd) {
 }
 
 void cb_await_all() {
-    for(int i = 0; i < cb_ctx.procs_count; i++) {
+    for (int i = 0; i < cb_ctx.procs_count; i++) {
         WaitForSingleObject(cb_ctx.procs[i].process_handle, INFINITE);
         /* we have to close these otherwise, they leak memory */
-        CloseHandle(cb_ctx.procs[i].process_handle); 
+        CloseHandle(cb_ctx.procs[i].process_handle);
         CloseHandle(cb_ctx.procs[i].thread_handle);
     }
     cb_ctx.procs_count = 0;
 }
 
 void cb_reset(command *cmd) {
-    string prefix = STR_LIT("cmd.exe /c");
+    cb_string prefix = CB_STRING("cmd.exe /c");
     memcpy(cmd->value, prefix.value, prefix.len);
     cmd->len = prefix.len;
 }
@@ -151,7 +155,9 @@ void cb_rebuild_yourself_impl(int argc, char **argv, const char *source_path) {
     const char *binary_path = argv[0];
 
     const char *inputs[] = {source_path};
-    if (cb_needs_rebuild(binary_path, inputs, 1) <= 0) return;
+    if (cb_needs_rebuild(binary_path, inputs, 1) <= 0) {
+        return;
+    }
 
     char old_binary_path[256];
     snprintf(old_binary_path, sizeof(old_binary_path), "%s.old", binary_path);
@@ -163,10 +169,10 @@ void cb_rebuild_yourself_impl(int argc, char **argv, const char *source_path) {
 
     command cmd = {0};
     cb_cc(&cmd);
-    cb_append(&cmd, STR_LIT("-g"));
-    cb_append(&cmd, (string){.value = (char *)source_path, .len = strlen(source_path)});
-    cb_append(&cmd, STR_LIT("-lkernel32 -luser32 -lshell32 -o"));
-    cb_append(&cmd, (string){.value = (char *)binary_path, .len = strlen(binary_path)});
+    cb_append(&cmd, CB_STRING("-g"));
+    cb_append(&cmd, (cb_string){.value = (char *)source_path, .len = strlen(source_path)});
+    cb_append(&cmd, CB_STRING("-lkernel32 -luser32 -lshell32 -o"));
+    cb_append(&cmd, (cb_string){.value = (char *)binary_path, .len = strlen(binary_path)});
 
     if (!cb_run(&cmd)) {
         MoveFileExA(old_binary_path, binary_path, MOVEFILE_REPLACE_EXISTING);
@@ -174,7 +180,7 @@ void cb_rebuild_yourself_impl(int argc, char **argv, const char *source_path) {
     }
 
     cb_reset(&cmd);
-    cb_append(&cmd, (string){.value = (char *)binary_path, .len = strlen(binary_path)});
+    cb_append(&cmd, (cb_string){.value = (char *)binary_path, .len = strlen(binary_path)});
     cb_run(&cmd);
     ExitProcess(0);
 }
@@ -218,7 +224,7 @@ int cb_needs_rebuild(const char *binary_path, const char **source_paths, size_t 
 
 #else /* POSIX */
 /* TODO: Implement POSIX */
-#inlcude <sys/stat.h>
+#inlcude < sys / stat.h>
 
 void cb_reset(command *cmd) {
     cmd->len = 0;
